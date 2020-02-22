524821942f0e603583e6edaf9dd01a2fd97f1a4a
Konstantin Suvorov <47966800+k-a-suvorov@users.noreply.github.com>
2020-02-22 21:04:59 +0300

    Add files via upload
    Fix some errors

--- lindice.py
+++ lindice.py
@@ -8,7 +8,7 @@ import random
 import datetime
 
 #Библиотека раскрашивания символов в линукс-консоли
-from termcolor import colored, cprint 
+import termcolor
 
 
 #Объявление переменных
@@ -18,22 +18,21 @@ currency = "$"
 win = False
 Loose = False
 
-text = ''
-color = ''
+text = ""
 
 #Функции раскраски текста
 
 def colorPosixLine(text): #Linux color text
-	cprint(text, 'yellow')
+	termcolor.cprint(text, 'yellow')
 	
 def colorWin(text): #Linux color text
-	cprint(text, 'green')
+	termcolor.cprint(text, 'green')
 			
 def colorLoose(text): #Linux color text
-	cprint(text, 'orange')
+	termcolor.cprint(text, 'red')
 	
 def colorChoice(text): #Linux color text
-	cprint(text, 'blue')	
+	termcolor.cprint(text, 'blue')	
 
 #Функция покера на костях, пока без анимации	
 #конструкция if elif громоздкая, так как нужно проверить множество выигрышных условий	
@@ -54,115 +53,115 @@ def getDice():
 	if (a == b == x == y == z):
 		print('Покер!')
 		win = True
-		result = money + 1000
+		result = money + 100
 		colorWin(F'Вы выиграли комбинацию покер. Ваш выигрыш составил {result, currency}')
 		choice()
 	elif ((a == y == z) or (a == x == y) or (a == y == z)):
 		print('три одинаковых!')
 		win = True
-		result = money + 600
+		result = money + 60
 		colorWin(F'Вы выиграли комбинацию Три одинаковых. Ваш выигрыш составил {result, currency}')
 		choice()
 	elif ((a == b == z) or (a == b == y) or (a  == b == z)):
 		print('три одинаковых!')	
 		win = True
-		result = money + 600
+		result = money + 60
 		colorWin(F'Вы выиграли комбинацию Три одинаковых. Ваш выигрыш составил {result, currency}')
 		choice()
 	elif ((b == x == y) or (b == x == z) or (b  == y == z)):
 		print('три одинаковых!')	
 		win = True
-		result = money + 600
+		result = money + 60
 		colorWin(F'Вы выиграли комбинацию Три одинаковых. Ваш выигрыш составил {result, currency}')
 		choice()
 	elif ((a == b) or (b == x) or (x == y) or (y == z) or (a == x) or (a == y) or (a == z)):
 		print('Одна пара!')
 		win = True
-		result = money + 50
+		result = money + 5
 		colorWin(F'Вы выиграли комбинацию Одна пара. Ваш выигрыш составил {result, currency}')
 		choice()
 	elif ((b == y) or (b == z) or (z == x)):
 		print('Одна пара!')	
 		win = True
-		result = money + 50
+		result = money + 5
 		colorWin(F'Вы выиграли комбинацию Одна пара. Ваш выигрыш составил {result, currency}')
 		choice()
 	elif (((a == b) and (x == y)) or ((b == x) and (y == z)) or ((a == x) and (x == z))):	
 		print('Две пары!')	
 		win = True
-		result = money + 150
+		result = money + 15
 		colorWin(F'Вы выиграли комбинацию Две пары. Ваш выигрыш составил {result, currency}')
 		choice()
 	elif ((a == x) and ((b == y) or (a == z))):	
 		print('Две пары!')
 		win = True
-		result = money + 150
+		result = money + 15
 		colorWin(F'Вы выиграли комбинацию Две пары. Ваш выигрыш составил {result, currency}')
 		choice()
 	elif (a == b == x == z):		
 		print('Четыре одинаковых!')
 		win = True
-		result = money + 500
+		result = money + 50
 		colorWin(F'Вы выиграли комбинацию Четыре одинаовых. Ваш выигрыш составил {result, currency}')
 		choice()
 	elif (a == y == b == x):		
 		print('Четыре одинаковых!')
 		win = True
-		result = money + 500
+		result = money + 50
 		colorWin(F'Вы выиграли комбинацию Четыре одинаовых. Ваш выигрыш составил {result, currency}')
 		choice()
 	elif (a == b == z == y):		
 		print('Четыре одинаковых!')
 		win = True
-		result = money + 500
+		result = money + 50
 		colorWin(F'Вы выиграли комбинацию Четыре одинаовых. Ваш выигрыш составил {result, currency}')
 		choice()
 	elif (a == z == b == x):		
 		print('Четыре одинаковых!')
 		win = True
-		result = money + 500
+		result = money + 50
 		colorWin(F'Вы выиграли комбинацию Четыре одинаовых. Ваш выигрыш составил {result, currency}')				
 		choice()
 	elif ((a != b != x != y != z) and (a != 6 and b != 6 and x != 6 and y != 6 and z != 6)):	
 		print ('Первый стрит')	
 		win = True
-		result = money + 300
+		result = money + 30
 		colorWin(F'Вы выиграли комбинацию Первый стрит. Ваш выигрыш составил {result, currency}')
 		choice()
 	elif ((a != b != x != y != z) and (a != 1 and b != 1 and x != 1 and y != 1 and z != 1)):	
 		print ('Второй стрит')
 		win = True
-		result = money + 300
+		result = money + 30
 		colorWin(F'Вы выиграли комбинацию Второй стрит. Ваш выигрыш составил {result, currency}')
 		choice()
 	elif ((a == b == x) and (y == z)):
 		print ('Флешрояль!')
 		win = True
-		result = money + 700
+		result = money + 70
 		print(F'Вы выиграли комбинацию Флешрояль. Ваш выигрыш составил {result, currency}')	
 		choice()
 	elif ((a == x == y) and (z == b)):
 		print ('Флешрояль!')	
 		win = True
-		result = money + 700
+		result = money + 70
 		colorWin(F'Вы выиграли комбинацию Флешрояль. Ваш выигрыш составил {result, currency}')	
 		choice()
 	elif ((a == y == z) and (b == x)):
 		print ('Флешрояль!')
 		win = True
-		result = money + 700
+		result = money + 70
 		colorWin(F'Вы выиграли комбинацию Флешрояль. Ваш выигрыш составил {result, currency}')				
 		choice()
 	elif ((a == b == z) and (y == x)):
 		print ('Флешрояль!')		
 		win = True
-		result = money + 700
+		result = money + 70
 		colorWin(F'Вы выиграли комбинацию Флешрояль. Ваш выигрыш составил {result, currency}')	
 		choice()
 	elif ((a == b == y) and (x == z)):
 		print ('Флешрояль!')
 		win = True
-		result = money + 700
+		result = money + 70
 		colorWin(F'Вы выиграли комбинацию Флешрояль. Ваш выигрыш составил {result, currency}')	
 		choice()
 	elif ((a != b != x != y != z)):
